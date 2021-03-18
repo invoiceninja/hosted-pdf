1bdf16eddc85c7bbb68dd39e804b488149e67525
David Bomba <turbo124@gmail.com>
2021-03-18 11:31:32 +1100

    Really simply hosted PDF generation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function pdf(Request $request)
+    {
+
+		$snappdf = new \Beganovich\Snappdf\Snappdf();
+
+		$pdf = $snappdf
+		    ->setHtml($request->input('html'))
+		    ->generate();
+
+    	return Response::make($pdf, 200, [
+		    'Content-Type' => 'application/pdf',
+		    'Content-Disposition' => 'inline; filename="ninja.pdf"'
+		]);
+    }
 }
