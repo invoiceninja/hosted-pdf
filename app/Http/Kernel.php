16ec2203cb6fdfbc836d3bc736c21a3c50b6aa38
David Bomba <turbo124@gmail.com>
2022-01-08 15:03:27 +1100

    Minor fixes for rate limits

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:500,1',
+            'throttle:5000,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
