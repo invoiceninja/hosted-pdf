bc9396413999615adf27c74c414e92e290af4e77
David Bomba <turbo124@gmail.com>
2024-08-09 15:38:55 +1000

    Minor fixes

--- app/Console/Commands/NinjaVersion.php
+++ app/Console/Commands/NinjaVersion.php
@@ -39,7 +39,7 @@ class NinjaVersion extends Command
     public function handle()
     {
         $version_file = trim(file_get_contents('https://raw.githubusercontent.com/invoiceninja/invoiceninja/v5-develop/VERSION.txt'));
-        Cache::put('version', $version_file);
+        Cache::forever('version', $version_file);
     }
 
 }
